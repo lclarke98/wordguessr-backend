96747353e40fb9418c8748c9a24a951313ea6ba8
Leo <leoclarke1998@gmail.com>
2021-03-27 20:38:48 +0000

    Added dev server

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-//const privateKey  = fs.readFileSync('key.pem', 'utf8');
-//const certificate = fs.readFileSync('cert.pem', 'utf8');
-//const credentials = {key: privateKey, cert: certificate};
+const privateKey  = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const express = require('express');
 const app = express();
 
 const cors = require('cors');
 
 // use it before all route definitions
-app.use(cors({origin: 'http://localhost:1024'}));
-
-//app.use(function(req, res, next) {
-//    console.log("TEST")
-//    res.header("Access-Control-Allow-Origin", "https://wordguessr.com"); // update to match the domain you will make the request from
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    next();
-//});
+app.use(function(req, res, next) {
+    console.log("TEST")
+    res.header("Access-Control-Allow-Origin", "https://wordguessr.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 
@@ -26,7 +24,7 @@ app.use('/user', require('./user'));
 
 // your express configuration here
 const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080);
-//httpsServer.listen(443);
+httpsServer.listen(443);
